3ad8c0a28f393f0c1e6cf334087aade9167e5f53
faisal <faisalabdalnasser275@gmail.com>
2025-02-13 22:45:54 +0200

    add marker to current position

--- lib/views/google_map_view.dart
+++ lib/views/google_map_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:route_tracker/utils/location_service.dart';
 
 class GoogleMapView extends StatefulWidget {
   const GoogleMapView({super.key});
@@ -12,17 +13,81 @@ class _GoogleMapViewState extends State<GoogleMapView> {
 
   late CameraPosition initalCameraPosition;
 
+  late LocationService locationService;
+
+  late GoogleMapController googleMapController;
+  Set<Marker> markers = {};
+
+
   @override
   void initState() {
     initalCameraPosition = const CameraPosition(
       target: LatLng(0,0),
       );
+
+      locationService = LocationService();
+      
     super.initState();
   }
+
+
+
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
+      markers: markers,
       zoomControlsEnabled: false,
-      initialCameraPosition: initalCameraPosition);
+      initialCameraPosition: initalCameraPosition,
+      onMapCreated: (controller) {
+        googleMapController = controller;
+        updatecurrentLocation();
+      },
+      );
+  }
+
+
+
+
+
+
+
+
+  
+  void updatecurrentLocation() async {
+
+    try {
+
+        var locationData = await locationService.getLocation();
+
+        LatLng currentPostion = LatLng(
+          locationData.latitude!, locationData.longitude!);
+
+          Marker currentLocationMarker = Marker(
+            markerId: MarkerId("my location"),
+            position: currentPostion,
+            );
+
+            markers.add(currentLocationMarker);
+
+            setState(() {});
+
+        CameraPosition myCurrentCameraPosition = CameraPosition(
+          target: currentPostion,
+          zoom: 16,
+          );
+
+        googleMapController.animateCamera(
+          CameraUpdate.newCameraPosition(myCurrentCameraPosition),
+          );
+
+  } on LocationServiceException catch (e) {
+  
+  
+  } on LocationPermissionException catch(e){
+
+  } catch(e){
+
+  }
+    
   }
 }
